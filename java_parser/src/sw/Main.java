41d4f6dd60675a5cd9d1ef35cec133c6f4101b8d
김현태 <76264568+KIM-HYUNTAE@users.noreply.github.com>
2023-12-06 20:05:41 +0900

    Add files via upload

---
+++ java_parser/src/sw/Main.java
@@ -0,0 +1,11 @@
+package sw;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("aa");
+		//JavaParserPrac jp = new JavaParserPrac();		
+		JavaParser jp = new JavaParser();
+		//ClassDiagram cd = new ClassDiagram();
+	}
+
+}
